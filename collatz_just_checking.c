6cc267ccd66c548a9957d7c16c76df8f500fd5b6
MadSecond <155824005+MadSecond@users.noreply.github.com>
2024-01-06 14:36:22 +0100

    Add files via upload
    No output of the numbers, only the checking is done. Code is simply the same without output.

---
+++ collatz_just_checking.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <gmp.h>
+#include <time.h>
+#include <unistd.h>
+
+#define SLEEP_TIME 200000
+
+int main(){
+
+    mpz_t n;
+    mpz_t x;
+    mpz_init(n);
+    mpz_init(x);
+
+    unsigned long int base;
+    unsigned long int exponent;
+    clock_t start, end;
+    double cpu_time_used;
+
+    printf("Please enter base of the starting number: ");
+    scanf(" %lu", &base);
+
+    printf("\nPlease enter exponent of the starting number: ");
+    scanf(" %lu", &exponent);
+
+    mpz_ui_pow_ui(x, base, exponent);
+
+label:
+    mpz_set(n, x);
+    mpz_sub_ui(n, n, 1);
+    printf("Checking (%lu^%lu)-1 ...  ", base, exponent);
+    start = clock();
+
+    while(mpz_cmp_ui(n, 1) != 0){
+        if(mpz_even_p(n) != 0){
+            mpz_divexact_ui(n, n, 2);
+            }
+        else{
+        mpz_mul_ui(n, n, 3);
+        mpz_add_ui(n, n, 1);
+        };
+    }
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("ENDS IN A LOOP! time taken to calculate: %lf seconds\n", cpu_time_used);
+    exponent++;
+    mpz_mul_ui(x, x, base);
+    goto label; //Can be made into a while(1) loop, but one goto in small code is fine
+
+return 0;
+}
